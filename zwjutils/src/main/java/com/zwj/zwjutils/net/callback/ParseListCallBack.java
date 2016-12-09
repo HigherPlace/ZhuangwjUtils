1f38369cd640174184e5b39b95ce7b8d8be2c045
a404318964 <winking13>
2016-12-09 15:25:15 +0800

    修改网络工具模块 1.新增上传文件接口（单文件上传、多文件上传） 2.新增实体解析回调接口 3.新增自定义扩展解析接口

--- zwjutils/src/main/java/com/zwj/zwjutils/net/callback/ParseListCallBack.java
+++ zwjutils/src/main/java/com/zwj/zwjutils/net/callback/ParseListCallBack.java
@@ -2,6 +2,7 @@ package com.zwj.zwjutils.net.callback;
 
 import android.text.TextUtils;
 
+import com.zwj.zwjutils.JsonUtil;
 import com.zwj.zwjutils.net.bean.ResponseBean;
 
 import java.util.List;
@@ -11,12 +12,20 @@ import java.util.List;
  */
 
 public abstract class ParseListCallback<T> extends SimpleCallBack{
+    private Class<T> clazz;
+
+    public ParseListCallback(Class<T> clazz) {
+        super();
+        this.clazz = clazz;
+    }
 
     @Override
     public void onSuccess(ResponseBean responseBean) {
+        // TODO 还需处理一些类型转换出错等之类的错误，抛出异常
         String result = responseBean.getResult();
-        if(!TextUtils.isEmpty(result)) {
-
+        if (!TextUtils.isEmpty(result)) {
+            List<T> list = JsonUtil.getObjects(result, clazz);
+            onSuccess(responseBean, list);
         }
     }
 
