ff8a21db4bc9e555b5f399bc5ae9436e94f061d5
a404318964 <winking13>
2017-12-04 11:22:05 +0800

    手机号验证工具，正则表达式支持166、198、199号码段

--- zwjutils/src/main/java/com/zwj/zwjutils/RegexUtils.java
+++ zwjutils/src/main/java/com/zwj/zwjutils/RegexUtils.java
@@ -27,7 +27,7 @@ public class RegexUtils {
     public static boolean checkPhone(String phone) {
 //        String regex = "(\\+\\d+)?(\\d{3,4}\\-?)?\\d{7,8}$";
 //        String regex = "^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\\d{8}$";
-        String regex = "^(13\\d|14[57]|15[^4,\\D]|17[678]|18\\d)\\d{8}|170[059]\\d{7}$";
+        String regex = "^(13\\d|14[57]|15[^4,\\D]|166|17[678]|18\\d|19[89])\\d{8}|170[059]\\d{7}$";
         return Pattern.matches(regex, phone);
     }
 
